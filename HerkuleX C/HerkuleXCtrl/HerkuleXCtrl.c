319a11ec921c3012199d6822c12a34d20b12758c
0ljik <oljaktl@gmail.com>
2018-12-14 00:30:14 +0600

    first check

---
+++ HerkuleX C/HerkuleXCtrl/HerkuleXCtrl.c
@@ -0,0 +1,147 @@
+﻿//CPU 클럭 설정
+#define F_CPU 16000000
+
+//ATmega128의 레지스터 등이 정의되어 있음
+#include <avr/io.h>
+//interrupt를 사용하는 데 필요한 정보들이 정의되어 있음
+#include <avr/interrupt.h>
+//_delay_ms 등 딜레이 함수가 정의되어 있음
+#include <util/delay.h>
+
+//herkulex 라이브러리의 헤더 파일을 포함
+#include <herkulex.h>
+//#include <USART0.h>
+#include <TIMER0.h>
+
+//DRS-0101의 Calibrated Position 정보를 사용하기 위한 구조체
+typedef struct DrsCaliPos
+{
+	int				iPosition : 13;
+	unsigned int	uiGPIO1 : 1;
+	unsigned int	uiGPIO2 : 1;
+	unsigned int	reserved : 1;
+}DrsCaliPos;
+
+//Calibrated Position 정보를 쉽게 다루기 위한 공용체
+typedef union DrsUnionCaliPos
+{
+	DrsCaliPos		stCaliPos;
+	unsigned int	uiCaliPos;
+}DrsUnionCaliPos;
+
+int main(void)
+{
+	//보낼 패킷과 받을 패킷이 저장될 패킷 구조체 선언
+	DrsPacket stSendPacket, stRcvPacket;
+	//Calibrated Position을 저장할 공용체 변수 선언
+	DrsUnionCaliPos unCaliPos;
+	//사용할 변수들 선언
+	unsigned char ucResult;
+	int iPos = 400;
+	
+	//포트 C의 입출력 방향 설정
+	DDRC = 0b01111111;
+	//포트 C의 출력 값 설정
+	PORTC = 0b01111111;
+	
+	//전체 인터럽트를 비활성화
+	cli();
+	
+	//HerkuleX를 사용하기 위해 초기화
+	hklx_Init(115200);
+	
+	//전체 인터럽트를 활성화
+	sei();
+	
+	//Torque Control에 0x60을 써서 토크를 거는 패킷 구성
+	stSendPacket.ucPacketSize = MIN_PACKET_SIZE + 3;
+	stSendPacket.ucChipID = 253;
+	stSendPacket.ucCmd = CMD_RAM_WRITE;
+	stSendPacket.unData.stRWData.ucAddress = 52;
+	stSendPacket.unData.stRWData.ucLen = 1;
+	stSendPacket.unData.stRWData.ucData[0] = 0x60;
+	
+	//패킷 보내기
+	hklx_SendPacket(stSendPacket);
+	
+	while(1)
+	{
+		//목표 위치 변수 값 변경
+		if(iPos == 400){
+			iPos = 624;
+		}			
+		else{
+			iPos = 400;
+		}			
+		
+		//DRS-0101을 움직일 I_JOG 패킷 구성
+		stSendPacket.ucPacketSize = MIN_PACKET_SIZE + CMD_I_JOG_STRUCT_SIZE;
+		stSendPacket.ucChipID = BROADCAST_ID;
+		stSendPacket.ucCmd = CMD_I_JOG;
+		stSendPacket.unData.stIJogData.stIJog[0].stJog.uiValue = iPos;
+		stSendPacket.unData.stIJogData.stIJog[0].stSet.ucStopFlag = 0;
+		stSendPacket.unData.stIJogData.stIJog[0].stSet.ucMode = 0;
+		stSendPacket.unData.stIJogData.stIJog[0].stSet.ucLedGreen = 1;
+		stSendPacket.unData.stIJogData.stIJog[0].stSet.ucLedBlue = 1;
+		stSendPacket.unData.stIJogData.stIJog[0].stSet.ucLedRed = 1;
+		stSendPacket.unData.stIJogData.stIJog[0].stSet.ucJogInvalid = 0;
+		stSendPacket.unData.stIJogData.stIJog[0].ucId = 253;
+		stSendPacket.unData.stIJogData.stIJog[0].ucPlayTime = 50;
+		
+		//패킷 보내기
+		hklx_SendPacket(stSendPacket);
+	
+		//다 움직일 때 까지 1초 대기
+		_delay_ms(1000);
+		
+		//Calibrated Position을 읽어올 RAM_READ 패킷 구성
+		stSendPacket.ucPacketSize = MIN_PACKET_SIZE + 2;
+		stSendPacket.ucChipID = 253;
+		stSendPacket.ucCmd = CMD_RAM_READ;
+		stSendPacket.unData.stRWData.ucAddress = 58;
+		stSendPacket.unData.stRWData.ucLen = 2;
+		
+		//패킷 보내기
+		hklx_SendPacket(stSendPacket);
+		
+		//TIMEOUT 까지 기다릴 시간 설정. 2*15 = 30(ms)
+		gucTimerTick=15;
+		while(1){
+			//패킷을 받는 함수를 호출해 결과를 ucResult에 저장
+			ucResult = hklx_ucReceivePacket(&stRcvPacket);
+			//결과 값이 DRS_RXWAITING이 아니면 빠져나옴
+			if(ucResult != DRS_RXWAITING){
+				break;
+			}
+			//30ms가 지나서 gucTimerTick이 0이 되면 빠져나옴
+			if(gucTimerTick==0){
+				ucResult = DRS_RXTIMEOUT;
+				break;
+			}				
+		}
+		
+		//패킷 수신이 정상적으로 완료 시
+		if(ucResult == DRS_RXCOMPLETE){
+			//받은 데이터 2바이트를 공용체 변수에 저장
+			unCaliPos.uiCaliPos = stRcvPacket.unData.stRWData.ucData[0] | 
+								 (stRcvPacket.unData.stRWData.ucData[1]<<8);
+			
+			//목표 위치와 5 이내로 근접했으면 LED 모두 끔
+			if((iPos - unCaliPos.stCaliPos.iPosition) > -5 && 
+			   (iPos - unCaliPos.stCaliPos.iPosition) < 5){
+				PORTC = 0b01111111;	
+			}
+			//목표 위치의 5 밖으로 나갔으면 LED 모두 켬
+			else{
+				PORTC = 0b00000000;
+			}				
+		}
+		//패킷 수신이 정상적으로 이루어지지 않았을 때 LED 모두 켬
+		else{
+			PORTC = 0b00000000;
+		}			
+	}
+	
+	//함수의 형태에 맞게 정수값 반환
+	return 1;
+}
--- xsherkulex.py
+++ xsherkulex.py
@@ -7,6 +7,58 @@ class herkulex(object):
         super(herkulex, self).__init__()
         self.port = port
         self.baud = baud
+        __DATA_SIZE=30		# buffer for input data
+        __DATA_MOVE=50		# max 10 servos <---- change this for more servos!
+        __TIME_OUT=5   	#timeout serial communication
+
+        # SERVO HERKULEX COMMAND - See Manual p40
+        __HEEPWRITE=0x01 	#Rom write
+        __HEEPREAD=0x02 	#Rom read
+        __HRAMWRITE=0x03 	#Ram write
+        __HRAMREAD=0x04 	#Ram read
+        __HIJOG=0x05 	#Write n servo with different timing
+        __HSJOG=0x06 	#Write n servo with same time
+        __HSTAT=0x07 	#Read error
+        __HROLLBACK=0x08 	#Back to factory value
+        __HREBOOT=0x09 	#Reboot
+
+        # HERKULEX LED - See Manual p29
+        __LED_GREEN =	 0x01
+        __LED_BLUE  =   0x02
+        __LED_CYAN  =   0x03
+        __LED_RED   = 	 0x04
+        __LED_GREEN2= 	 0x05
+        __LED_PINK  =   0x06
+        __LED_WHITE =   0x07
+
+        # HERKULEX STATUS ERROR - See Manual p39
+        __H_STATUS_OK					= 0x00
+        __H_ERROR_INPUT_VOLTAGE 		= 0x01
+        __H_ERROR_POS_LIMIT			= 0x02
+        __H_ERROR_TEMPERATURE_LIMIT	= 0x04
+        __H_ERROR_INVALID_PKT			= 0x08
+        __H_ERROR_OVERLOAD			= 0x10
+        __H_ERROR_DRIVER_FAULT  		= 0x20
+        __H_ERROR_EEPREG_DISTORT		= 0x40
+
+        # HERKULEX Broadcast Servo ID
+        __BROADCAST_ID = 0xFE
+
+
+        _pSize = 0
+        _pID = 0
+        _cmd = 0
+        _lengthString = 0
+        _ck1 = 0
+
+        _conta = 0
+
+        _XOR = 0
+        _playTime = 0
+        __data=bytearray(self.__DATA_SIZE)
+        __dataEx=bytearray(self.__DATA_MOVE+8)
+        __moveData=bytearray(self.__DATA_MOVE)
+
     @classmethod
     def connect(cls, id):
         self.ser=appserial.connect(port,baud)
@@ -16,8 +68,96 @@ class herkulex(object):
     def getid(brd_id):
         return brd_id"""
 
+    def initialize():
+        conta=0
+        lenghtString=0
+        sleep(0.1)
+        clearError(brd_id)
+        sleep(0.01)
+        ACK(1)
+        slep(0.01)
+        torqueON(brd_id)
+        sleep(0.01)
+
     @classmethod
     def getid(cls, brd_id):
         appserial.send()
         input=appserial.read()
         return cls()
+
+    def ACK(int valueACK):
+    	pSize = 0x0A               # 3.Packet size 7-58
+    	pID   = 0xFE	            # 4. Servo ID
+    	cmd   = HRAMWRITE          # 5. CMD
+    	data[0]=0x34               # 8. Address
+    	data[1]=0x01               # 9. Lenght
+    	data[2]=valueACK           # 10.Value. 0=No Replay, 1=Only reply to READ CMD, 2=Always reply
+    	lenghtString=3             # lenghtData
+
+    	ck1=checksum1(data,lenghtString)	#6. Checksum1
+    	ck2=checksum2(ck1)					#7. Checksum2
+
+    	dataEx[0] = 0xFF			# Packet Header
+    	dataEx[1] = 0xFF			# Packet Header
+    	dataEx[2] = pSize	 		# Packet Size
+    	dataEx[3] = pID			# Servo ID
+    	dataEx[4] = cmd			# Command Ram Write
+    	dataEx[5] = ck1			# Checksum 1
+    	dataEx[6] = ck2			# Checksum 2
+    	dataEx[7] = data[0] 		# Address 52
+    	dataEx[8] = data[1] 		# Length
+    	dataEx[9] = data[2] 		# Value
+
+     	sendData(dataEx, pSize)
+
+    def clearError(int servoID):
+    	pSize = 0x0B               # 3.Packet size 7-58
+    	pID   = servoID     		# 4. Servo ID - 253=all servos
+    	cmd   = HRAMWRITE          # 5. CMD
+    	data[0]=0x30               # 8. Address
+    	data[1]=0x02               # 9. Lenght
+    	data[2]=0x00               # 10. Write error=0
+    	data[3]=0x00               # 10. Write detail error=0
+
+    	lenghtString=4             # lenghtData
+
+    	ck1=checksum1(data,lenghtString)	#6. Checksum1
+    	ck2=checksum2(ck1)					#7. Checksum2
+
+    	dataEx[0] = 0xFF			# Packet Header
+    	dataEx[1] = 0xFF			# Packet Header
+    	dataEx[2] = pSize	 		# Packet Size
+    	dataEx[3] = pID			# Servo ID
+    	dataEx[4] = cmd			# Command Ram Write
+    	dataEx[5] = ck1			# Checksum 1
+    	dataEx[6] = ck2			# Checksum 2
+    	dataEx[7] = data[0] 		# Address 52
+    	dataEx[8] = data[1] 		# Length
+    	dataEx[9] = data[2] 		# Value1
+    	dataEx[10]= data[3] 		# Value2
+
+    	sendData(dataEx, pSize)
+
+
+    def checksum1(byte* data, int lenghtString):
+        XOR = 0
+        XOR = XOR ^ pSize
+        XOR = XOR ^ pID
+        XOR = XOR ^ cmd
+        for i in range(lenghtString):
+            XOR = XOR ^ data[i]
+        return XOR&0xFE
+
+    def checksum2(int XOR):
+      return (~XOR)&0xFE
+
+    def sendData(byte* buffer, int lenght):
+        clearBuffer() 		#clear the serialport buffer - try to do it!
+        Serial1.write(buffer, lenght)
+        sleep(0.001)
+
+    def clearBuffer()
+		Serial1.flush();
+		while (Serial1.available()){
+		Serial1.read();
+		delayMicroseconds(200);
